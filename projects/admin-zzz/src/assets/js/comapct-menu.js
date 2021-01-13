22eb38926e88af127afc930596cd6929955b75e7
anhph9492 <phamhoanganh9492@gmail.com>
2021-01-13 14:09:25 +0700

    ok

--- projects/admin-zzz/src/assets/js/comapct-menu.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-$(window).bind("resize", function () {
-        console.log($(this).width())
-        if ($(this).width() < 1024) {
-            $('.pages-body-wrapper').removeClass('sidebar-icon');
-        } else {
-            $('.pages-body-wrapper').addClass('sidebar-icon')
-        }
-    }).trigger('resize');
